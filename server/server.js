2afdfe3f18560bebd5ea4bd6d36210d9e7092e28
ShreyanBiswas <101624966+ShreyanBiswas@users.noreply.github.com>
2023-02-03 22:19:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -48,6 +48,6 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-    console.log("App listening on port: ", 5000)
+app.listen(6000, () => {
+    console.log("App listening on port: ", 6000)
 })
